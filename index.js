ca775fb69e71c7ff0cdb49b7df6fecc3a0343b54
Vishal <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-09-23 13:46:58 +0530

    Changes in JS

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
+const errorMessage = document.querySelector("#error-message");
+
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    if(billAmount.value>0) {
+
+    }
+    else {
+         errorMessage.innerText = "The bill amount should be greater than 0"
+    }
+});
