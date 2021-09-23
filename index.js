5def550547fbdc54adedcc247455e6020e9644bf
Vishal <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-09-23 16:06:23 +0530

    Added data of notes

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const errorMessage = document.querySelector("#error-message");
+const noOfNotes = document.querySelector(".no-of-notes")
 
+const availableNotes  = [2000,500,100,20,10,5,1]
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     errorMessage.style.display ="none";
@@ -11,6 +13,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
             if(cashGiven.value >= billAmount.value ){
                 const amountToBeReturned = cashGiven - billAmount;
                 calculateChange(amountToBeReturned);
+                
             }
             else 
             {
@@ -23,8 +26,12 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
     }
 });
 
-function calculateChange() {
-    
+function calculateChange(amountToBeReturned) {
+    for(let i = 0; i<availableNotes.length;i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturned /availableNotes[i]);
+        amountToBeReturned %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
 }
 
 function showMessage(message) {
