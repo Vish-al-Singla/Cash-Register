6e13670c7f52c245b965ab6411c766aca5fcbe18
Vishal <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-09-23 16:20:26 +0530

    Added data of notes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const errorMessage = document.querySelector("#error-message");
-const noOfNotes = document.querySelector(".no-of-notes")
+const noOfNotes = document.querySelector(".no-of-notes");
 
-const availableNotes  = [2000,500,100,20,10,5,1]
+const availableNotes  = [2000,500,100,20,10,5,1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     errorMessage.style.display ="none";
@@ -13,7 +13,6 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
             if(cashGiven.value >= billAmount.value ){
                 const amountToBeReturned = cashGiven - billAmount;
                 calculateChange(amountToBeReturned);
-                
             }
             else 
             {
